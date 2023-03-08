ce6297c03ba3cbd8b75205e677401d6dd784158e
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-08 22:52:16 +0300

    combi

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(10):
     for j in range(10):
         if j < i or j == i:
             continue
-        if i == 0 and j == 1:
-            print("{first}{last}".format(first=i, last=j), end='')
+        if i+1 == 9 and j+1 == 10:
+            print("{first}{last}".format(first=i, last=j))
             continue
-        print(", {first}{last}".format(first=i, last=j), end='')
+        print("{first}{last}, ".format(first=i, last=j), end='')
