e32113ea8ec484d50c95abd6a1e52d7d956a67a3
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-08 21:30:03 +0300

    correcttt

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{number:02d}\n".format(number=i))
+        print("{number:02d}".format(number=i))
         continue
     print("{number:02d}, ".format(number=i), end='')
