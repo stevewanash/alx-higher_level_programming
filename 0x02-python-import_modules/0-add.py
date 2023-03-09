f00e0a6062b9baee82aa6a528b2fac1437ff9dda
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 17:50:19 +0300

    eeer

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
 if __name__ == "__main__":
-    import add_0
-    a = 1
-    b = 2
-    print("{n1} + {n2} = {n3}".format(n1=a, n2=b, n3=add_0.add(a, b)))
+    print("{n1} + {n2} = {n3}".format(n1=a, n2=b, n3=add(a, b)))
