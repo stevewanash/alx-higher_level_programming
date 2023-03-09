cd0f04118fa45a30757d925c715f72293299eaaf
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 17:27:13 +0300

    eeee

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,4 @@
 from add_0 import add
 a = 1
 b = 2
-print("{num1} + {num2} = {num3}".format(num1=a, num2=b, num3=add(1, 2)))
+print("{num1} + {num2} = {num3}".format(num1=a, num2=b, num3=add(a, b)))
