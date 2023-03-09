40420ecaf8397799471343ca75519787d3c5908e
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 18:19:40 +0300

    yyyy

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 counter = 1
-index = 1
+index = 0
 if __name__ == "__main__":
     for i in sys.argv:
         print("{pos}: {arg}".format(pos=counter, arg=sys.argv[index]))
         counter = +1
+        index = +1
     if i == 1:
         print("{pos}.".format(pos=counter))
