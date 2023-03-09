ae87365cf0372cfcf17b1ccfc9baf0c956a03e8e
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 17:43:02 +0300

    ere

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,5 @@
 import add_0
 a = 1
 b = 2
-print("{num1} + {num2} = {num3}".format(num1=a, num2=b, num3=add_0.add(a, b)))
+if __name__ == "__main__":
+    print("{n1} + {n2} = {n3}".format(n1=a, n2=b, n3=add_0.add(a, b)))
