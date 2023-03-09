eba284f55defa3531782710c75098ae5a5f47f64
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 18:55:10 +0300

    iiii

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,13 @@ import sys
 counter = 1
 if __name__ == "__main__":
     no_of_arg = len(sys.argv)
-    if no_of_arg == 0:
-        print("{args} arguments:".format(args=no_of_arg))
-        print("{pos}.".format(pos=counter))
-    elif no_of_arg == 1:
+    if no_of_arg == 1:
         print("{args} argument:".format(args=no_of_arg))
-        for i in range(no_of_arg - 1):
-            print("{pos}: {arg}".format(pos=counter, arg=sys.argv[i]))
-            counter = +1
     else:
         print("{args} arguments:".format(args=no_of_arg))
-        for i in range(no_of_arg - 1):
+    if no_of_arg == 0:
+        print("{pos}.".format(pos=counter))
+    else:
+        for i in range(no_of_arg):
             print("{pos}: {arg}".format(pos=counter, arg=sys.argv[i]))
             counter = +1
