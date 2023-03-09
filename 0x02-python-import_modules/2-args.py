b0d9267e2711f17a18517ac965ab448226a33b8e
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-09 19:06:34 +0300

    sss

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     no_of_arg = len(sys.argv)
     if no_of_arg == 1:
         print("{args} argument:".format(args=no_of_arg))
+    elif no_of_arg == 0:
+        print("{args} arguments.".format(args=no_of_arg))
     else:
         print("{args} arguments:".format(args=no_of_arg))
-    if no_of_arg == 0:
-        print("{pos}.".format(pos=counter))
-    else:
+    if no_of_arg > 0:
         for i in range(no_of_arg):
             print("{pos}: {arg}".format(pos=counter, arg=sys.argv[i]))
             counter = +1
