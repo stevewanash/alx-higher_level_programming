74e4204f8bf0f92ea4a58e3cdaee04ca153ed236
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-13 00:46:38 +0300

    esssssss

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     no_args = len(sys.argv)
     for i in range(1, no_args):
         sum += int(sys.argv[i])
-    print("{result}".format(result=sum))
+    print("{result}".format(result=sum), end='')
