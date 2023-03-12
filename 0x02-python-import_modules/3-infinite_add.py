c8062b92cd8ccedda2e12e8654fb3fbb24f59024
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-13 00:45:21 +0300

    ererererer

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-sum = 0
 if __name__ == "__main__":
+    import sys
+    sum = 0
     no_args = len(sys.argv)
     for i in range(1, no_args):
         sum += int(sys.argv[i])
