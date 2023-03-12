47f709f387c7e7f38ef7dcdad967938723d116c8
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-13 01:49:43 +0300

    gjgjg

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4
+    names = dir(hidden_4)
+    no_names = len(dir(hidden_4))
+    for i in range(no_names):
+        current = names[i]
+        if current[0] == '_':
+            continue
+        print("{name}".format(name=current))
