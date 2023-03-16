c6ceff566940f640cbf2a0c976441c0d63c3fe75
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-16 04:32:56 +0300

    tttt

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary is not None:
         list_scores = [a_dictionary[i] for i in a_dictionary]
         key_list = list(a_dictionary)
-
         return key_list[list_scores.index(max(list_scores))]
     else:
         return None
