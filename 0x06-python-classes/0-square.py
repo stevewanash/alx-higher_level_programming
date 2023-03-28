4cec77944c72f8fdd71178f77621220342943671
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-29 00:36:10 +0300

    lll

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    This is a class that defines a square
-
-    Attributes:
-        pass
-    """
+    """This is a class that defines a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """
-    This class defines a square
-    """
+    """This class defines a square"""
+
     def __init__(self, size):
-        """
-        This function creates instances for the attribute size
+        """This function creates instances for the attribute size
 
         Args:
-            size (int): size of the square's sides, without units
+            size: size of the square's sides, without units
+
         """
         self.__size = size
