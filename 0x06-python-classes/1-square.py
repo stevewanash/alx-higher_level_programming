6d230047c542a58db6957de9c7cd80d045326f8a
Steve Wanangwe <stevewanash1@gmail.com>
2023-03-29 00:01:51 +0300

    kkk

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class defines a square
+    """
+    def __init__(self, size):
+        """
+        This function creates instances for the attribute size
+
+        Args:
+            size (int): size of the square's sides, without units
+        """
+        self.__size = size
