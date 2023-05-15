9a51348cd81915f6aa03a86dc5b7731c8c99a78f
Steve Wanangwe <stevewanash1@gmail.com>
2023-05-15 04:59:13 +0300

    lll

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,5 @@ def write_file(filename="", text=""):
     Returns:
         int: number of characters written to file
     """
-    with open(filename, "r+", encoding='utf-8') as file:
+    with open(filename, "w", encoding='utf-8') as file:
         return (file.write(text))
