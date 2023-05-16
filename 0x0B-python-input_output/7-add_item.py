6297fdd4ec3e25e1e3358b1ca3a03ebcbe08ef3f
Steve Wanangwe <stevewanash1@gmail.com>
2023-05-17 02:36:15 +0300

    ggg

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,12 +4,10 @@
 import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 loadd = __import__('6-load_from_json_file').load_from_json_file
+
 name = "./add_item.json"
-
-with open(name, '+a', encoding='utf-8') as file:
-    new_list = loadd(name)
-    if len(sys.argv) > 1:
-        for i in range(1, len(sys.argv)):
-            new_list.append(sys.argv[i])
-
+new_list = loadd(name)
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        new_list.append(sys.argv[i])
     save(new_list, name)
