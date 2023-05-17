b0f1a5618d7c91260a4e3b66db37bc2542647f7f
Steve Wanangwe <stevewanash1@gmail.com>
2023-05-17 03:13:44 +0300

    jjj

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-"""input and output JSON
+#!/c/Users/pc/AppData/Local/Programs/Python/Python311/python
+"""uses json for input and output
 """
 import sys
 import os
 save = __import__('5-save_to_json_file').save_to_json_file
 loadd = __import__('6-load_from_json_file').load_from_json_file
 
-name = "./add_item.json"
+name = "add_item.json"
 new_list = []
-if not os.stat("./add_item.json").st_size == 0:
+if not os.stat("add_item.json").st_size == 0:
     new_list = loadd(name)
 if len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
