a7ac4337e6e53e2b14f0cd969e7d328849066c0c
Steve Wanangwe <stevewanash1@gmail.com>
2023-05-17 04:56:07 +0300

    sss

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""uses json for input and output
+"""
+
+
+class Student:
+    """student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialises student attributes
+
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictonary representation of instance
+
+        Args:
+            obj (class): instance of a class
+
+        Returns:
+            dict: dictionary representation of the
+            instance
+        """
+        if attrs:
+            new_dict = {key: self.__dict__[key] for key in self.__dict__ if
+                        key in attrs}
+            return new_dict
+        else:
+            return self.__dict__
